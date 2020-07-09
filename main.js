f5b8bda1547e12ec39229ef8f8500431b8a7230f
sunshine274 <jingx274@gmail.com>
2020-07-09 09:16:51 +0000

    Added main documents

---
+++ main.js
@@ -0,0 +1,46 @@
+const baseURL =
+  "https://cors-anywhere.herokuapp.com/https://ci-swapi.herokuapp.com/api/";
+
+function getData(type, cb) {
+  let xhr = new XMLHttpRequest();
+
+  xhr.open("GET", baseURL + type + "/");
+  xhr.send();
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState == 4 && this.status == 200) {
+      cb(JSON.parse(this.responseText));
+    }
+  };
+}
+
+function getTableHeaders(obj){
+    let tableHeaders = [];
+    Object.keys(obj).forEach(function(key){
+        tableHeaders.push(`<td>${key}</td>`);
+    });
+    return `<tr>${tableHeaders}</tr>`;
+}
+
+function writeToDocument(type) {
+    let tableRows = [];
+  let el = document.getElementById("data");
+  el.innerHTML = "";
+
+  getData(type, function (data) {
+    data = data.results;
+    let tableHeaders = getTableHeaders(data[0]);
+    data.forEach(function (item) {
+        let dataRow = [];
+    
+        Object.keys(item).forEach(function(key){
+            let rowData = item[key].toString();
+            let truncatedData = rowData.substring(0,15);
+
+            dataRow.push(`<td>${truncatedData}</td>`);
+        });
+        tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+      el.innerHTML = `<table>${tableHeaders}&${tableRows}</table>`;
+    });
+}
