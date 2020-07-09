b3e21092c9eebeff8057107c33c6ceeedfcfb268
sunshine274 <jingx274@gmail.com>
2020-07-09 11:14:01 +0000

    Implement pagination

--- main.js
+++ main.js
@@ -1,46 +1,57 @@
-const baseURL =
-  "https://cors-anywhere.herokuapp.com/https://ci-swapi.herokuapp.com/api/";
-
-function getData(type, cb) {
+function getData(url, cb) {
   let xhr = new XMLHttpRequest();
-
-  xhr.open("GET", baseURL + type + "/");
-  xhr.send();
 
   xhr.onreadystatechange = function () {
     if (this.readyState == 4 && this.status == 200) {
       cb(JSON.parse(this.responseText));
     }
   };
+
+  xhr.open("GET", url);
+  xhr.send();
 }
 
-function getTableHeaders(obj){
-    let tableHeaders = [];
-    Object.keys(obj).forEach(function(key){
-        tableHeaders.push(`<td>${key}</td>`);
-    });
-    return `<tr>${tableHeaders}</tr>`;
+function getTableHeaders(obj) {
+  let tableHeaders = [];
+  Object.keys(obj).forEach(function (key) {
+    tableHeaders.push(`<td>${key}</td>`);
+  });
+  return `<tr>${tableHeaders}</tr>`;
 }
 
-function writeToDocument(type) {
-    let tableRows = [];
+function generatePaginationButtons(next, prev) {
+  if (next && prev) {
+    return `<button onclick="writeToDocument('${prev}')">Previous</button>
+            <button onclick="writeToDocument('${next}')">Next</button>`;
+  } else if (next && !prev) {
+    return `<button onclick="writeToDocument('${next}')">Next</button>`;
+  } else if (!next && prev) {
+    return `<button onclick="writeToDocument('${prev}')">Previous</button>`;
+  }
+}
+
+function writeToDocument(url) {
+  let tableRows = [];
   let el = document.getElementById("data");
-  el.innerHTML = "";
 
-  getData(type, function (data) {
+  getData(url, function (data) {
+    let pagination = "";
+    if (data.next || data.previous) {
+      pagination = generatePaginationButtons(data.next, data.previous);
+    }
     data = data.results;
     let tableHeaders = getTableHeaders(data[0]);
     data.forEach(function (item) {
-        let dataRow = [];
-    
-        Object.keys(item).forEach(function(key){
-            let rowData = item[key].toString();
-            let truncatedData = rowData.substring(0,15);
+      let dataRow = [];
 
-            dataRow.push(`<td>${truncatedData}</td>`);
-        });
-        tableRows.push(`<tr>${dataRow}</tr>`);
-        });
-      el.innerHTML = `<table>${tableHeaders}&${tableRows}</table>`;
+      Object.keys(item).forEach(function (key) {
+        let rowData = item[key].toString();
+        let truncatedData = rowData.substring(0, 15);
+
+        dataRow.push(`<td>${truncatedData}</td>`);
+      });
+      tableRows.push(`<tr>${dataRow}</tr>`);
     });
+    el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
+  });
 }
